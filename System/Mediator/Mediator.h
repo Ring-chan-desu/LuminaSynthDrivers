6d79459987d6bf5a796afe7eb2451f60e0b71727
Ring.✦ <jonathan_li233@outlook.com>
2026-05-01 00:00:10 +0800

    refactor(Mediator):重写了Topics

--- System/Mediator/Mediator.h
+++ System/Mediator/Mediator.h
@@ -5,6 +5,7 @@
 #include "main.h"
 #include "etl/map.h"
 #include "etl/vector.h"
+#include "stm32f407xx.h"
 
 #include <stdint.h>
 
@@ -17,8 +18,23 @@
 /* ---- 频道枚举 ----*/
 enum class Topics : uint8_t // 指定底层类型
 {
-    OSC_FM = 0,     // 振荡器调频
-    // OSC_AM,         // 振荡器调幅
+    // OSC_FM = 0,     // 振荡器调频
+    // // OSC_AM,         // 振荡器调幅
+    ADC_C1 = 0,
+    ADC_C2,
+    ADC_C3,
+    ADC_C4,
+    ADC_C5,
+    ADC_C6,
+    ADC_C7,
+    ADC_C8,
+    ADC_C9,
+    ADC_C10,
+    ADC_C11,
+    ADC_C12,
+    ADC_C13,
+    ADC_C14,
+    ADC_C15,
 };
 
 class Subscriber
@@ -30,10 +46,10 @@ class Subscriber
 class Mediator
 {
     private:
-        etl::map<Topics, etl::vector<Subscriber*, 2>, 2> SubscribersMap;
+        etl::map<Topics, etl::vector<Subscriber*, 10>, 10> SubscribersMap;
         Mediator() {}   //  单例一定要有私有的构造函数,不然会报错
     public:
-        static Mediator& GetInstance() {    //  麦耶斯单例
+        static Mediator& GetInstance() {    //  单例
             static Mediator Instance;   //  调用这个函数的时候才开始初始化,用的就是类私有的构造函数在内存中预留空间给实例
             return Instance;
             //  返回当前实例引用,可以通过这个函数访问,比如说 Mediator::GetInstance().Attribute = value;
@@ -43,12 +59,6 @@ class Mediator
             SubscribersMap[topic].push_back(subscriber);
         } // 订阅即注册频道
 
-        // void Mediator_Unsubscribe(Topics topic, Subscriber* subscriber){
-        //     auto it = SubscribersMap.find(topic);   // 先找找看
-        //     if (it != SubscribersMap.end()) {   //  看看有没有这个话题
-        //         it->second.remove(subscriber);
-        //     }
-        // }
 
         void Mediator_Publish(Topics topic, float value) {
             auto it = SubscribersMap.find(topic); // 先找找看
